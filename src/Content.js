2487e9760d65fcfbee71beafd1e862ba62f7c75f
abdullah <abdullasif1>
2022-07-16 18:15:10 +0500

    useState Hook

--- src/Content.js
+++ src/Content.js
@@ -1,41 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Content = () => {
+
+    const [name, setName] = useState('Bobby');
+    const [count, setCount] = useState(0);
+
     const handleNameChange = () => {
         const names = ["Gal Gaddot", "Nicky Minaj", "Lana Del Ray"];
         const int = Math.floor(Math.random() * 3);
-        return names[int];
+        setName(names[int]);
     }
 
     const handleClick = () => {
-        console.log("You clicked it")
+        setCount(count+1);  //this changes state for the next time to be shown
+        console.log(count); //the state we pass to function wont change itself in the function
     }
 
     const handleClick2 = (name) => {
         console.log(`${name} was clicked`)
     }
 
-    const handleClick3 = (e) => {
-        console.log(e.target.innerText)
-    }
-
     return (
         <main>
             <p onDoubleClick={handleClick}>
-                Hello {handleNameChange()}!
+                Hello {name}!
             </p>
             
-            {/* we cannot do this to change name as handleNameChange only renders when page loads, to solve this we will be using useState Hook*/}
-            <button onClick={handleNameChange}>Click on it to change the name</button>
+            <button onClick={handleNameChange}>Click on this to change the name</button>
 
-            {/* if we put () after handleClick in {handleClick} it would be invoked immediately without even being clicked  */}
             <button onClick={handleClick}>Click it</button>
             
-            {/* but here handleClick2 is not invoked immediately cause it it inside an anonymous function ()=>  */}
             <button onClick={() => handleClick2('Abdullah')}>Click it</button>
 
-            {/* this targets the event happened which is in this case is a button */}
-            <button onClick={(e) => handleClick3(e)}>Click it</button>
 
         </main>
     )
