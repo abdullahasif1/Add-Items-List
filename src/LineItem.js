f756199a1d8871131cf132b14a4de43e67f549bc
abdullah <abdullasif1>
2022-07-20 16:38:03 +0500

    Form and Controlled Input(Search Items added)

--- src/LineItem.js
+++ src/LineItem.js
@@ -3,7 +3,7 @@ import { FaTrashAlt } from 'react-icons/fa'
 
 const LineItem = ({ item, handleCheck, handleDelete }) => {
     return (
-        <li className='item' key={item.id}>
+        <li className='item'>
             <input type="checkbox" onChange={() => handleCheck(item.id)} checked={item.checked} />
             <label onDoubleClick={() => handleCheck(item.id)}>{item.item}</label>
             <FaTrashAlt onClick={() => handleDelete(item.id)} role="button" tabIndex="0" aria-label={`Delete ${item.item}`} />
